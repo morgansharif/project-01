aac908223c23bc6b99f2fc8274ec2583d40d1ffa
morgansharif <morgan.sharif@gmail.com>
2016-04-06 17:06:08 -0700

    continuing to assemble server folders

---
+++ controllers/apiController.js
@@ -0,0 +1,13 @@
+function index(req, res) {
+  res.json({
+    message: "Welcome to my project-01!",
+    // documentation_url: "github readme",
+    // base_url: "http://tunely.herokuapp.com",
+    // endpoints: [
+    //   {method: "GET", path: "/api", description: "Describes available endpoints"}
+    // ]
+  });
+}
+
+
+module.exports.index = index;
