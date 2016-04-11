4e6bad9ecd8dc5c73ba36b6fd07d66523a9b2f81
morgansharif <morgan.sharif@gmail.com>
2016-04-11 10:47:18 -0700

    ready to push to heroku

--- models/index.js
+++ models/index.js
@@ -1,5 +1,8 @@
 var mongoose = require('mongoose');
-mongoose.connect("mongodb://localhost/project-01");
+mongoose.connect( process.env.MONGOLAB_URI ||
+                  process.env.MONGOHQ_URL ||
+                  "mongodb://localhost/project-01");
+
 
 module.exports.Repo = require('./repo');
 module.exports.Snippet = require('./snippet');
